ab4b130d9b42456c730635a54863fde2360a5772
Paulo iziório <paulosp1995@gmail.com>
2018-08-31 15:00:54 -0300

    Pag compra

---
+++ src/main/java/model/Compra.java
@@ -0,0 +1,87 @@
+package model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Compra {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@OneToMany
+	private List<CadastroCliente> cliente;
+	
+	@ManyToMany
+	private List<Livro> livro;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public List<CadastroCliente> getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(List<CadastroCliente> cliente) {
+		this.cliente = cliente;
+	}
+
+	public List<Livro> getLivro() {
+		return livro;
+	}
+
+	public void setLivro(List<Livro> livro) {
+		this.livro = livro;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cliente == null) ? 0 : cliente.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((livro == null) ? 0 : livro.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Compra other = (Compra) obj;
+		if (cliente == null) {
+			if (other.cliente != null)
+				return false;
+		} else if (!cliente.equals(other.cliente))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (livro == null) {
+			if (other.livro != null)
+				return false;
+		} else if (!livro.equals(other.livro))
+			return false;
+		return true;
+	}
+	
+	
+
+}
